22c58188165d479aaceb33e842007b9fc387ae63
Meining Cheng <74748581+Meining2020@users.noreply.github.com>
2021-06-30 17:15:49 -0400

    add files

---
+++ index.js
@@ -0,0 +1,61 @@
+//button click
+const buttonElements = document.getElementsByClassName('drum');
+for (let i = 0; i < buttonElements.length; i++) {
+    buttonElements[i].addEventListener('click', function() {
+        let buttonText = this.textContent;
+        makeSound(buttonText);
+        buttonAnimation(buttonText);
+    })
+}
+
+//keyboard press
+document.addEventListener('keydown', function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function makeSound (text) {
+    switch (text) {
+        case 'w':
+            let tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case 'a':
+            let tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 's':
+            let tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'd':
+            let tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case 'j':
+            let snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case 'k':
+            let crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'l':
+            let kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(text) {
+    const activeButton = document.getElementsByClassName(text)[0];
+    activeButton.classList.add('pressed');
+
+    setTimeout(function() {
+        activeButton.classList.remove('pressed')
+    }, 100)
+
+}
+
